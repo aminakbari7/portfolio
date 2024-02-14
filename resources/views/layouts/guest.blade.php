4702829d4d962b6a1b400e3aa7c88d12ae0a13ba
aminakbari7 <aminakbari733@gmail.com>
2024-02-14 19:31:02 +0330

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,8 +17,9 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href=""{{ route('welcome') }}" class="logo" >
+                    <img style="
+                    background-blend-mode: multiply; border-radius:10px" src="assets/images/klassy-logo.png" align="klassy cafe html template">
                 </a>
             </div>
 
