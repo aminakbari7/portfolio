55d60fa232f40723a26aabcb8f26346b0e6d3aa5
aminakbari7 <aminakbari733@gmail.com>
2024-02-14 23:18:15 +0330

    Create createp.blade.php

---
+++ resources/views/livewire/createp.blade.php
@@ -0,0 +1,56 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="contact-form">
+                <form id="contact" action="" method="post" >
+                  <div class="row">
+                    <div class="col-lg-12">
+                        <h4>new project</h4>
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                      <fieldset>
+                        <input name="name" type="text" id="name" placeholder="tiltle*" required="">
+                      </fieldset>
+                    </div>
+
+
+
+                    <div class="col-lg-6 col-sm-12">
+                      <fieldset>
+                      <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="link" required="">
+                    </fieldset>
+                    </div>
+                    
+                    <div class="form-group col-lg-6 col-sm-12">
+                        <fieldset>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <option>which case</option>
+                          <option>machine learning</option>
+                          <option>web app</option>
+                        </select>
+                    </fieldset>
+
+                    <div class="col-lg-12">
+               
+                        <input type="file">
+                      </div>
+                 
+                    </div>
+
+
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <textarea name="message" rows="6" id="message" placeholder="body" required=""></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="main-button-icon">add</button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
