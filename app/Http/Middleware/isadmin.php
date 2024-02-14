53caff071c9b9e92e6689a84df64f672b9307620
aminakbari7 <aminakbari733@gmail.com>
2024-02-14 23:17:58 +0330

    Create isadmin.php

---
+++ app/Http/Middleware/isadmin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+
+class isadmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(auth()->user()->role=='admin')
+        return $next($request);
+    }
+}
