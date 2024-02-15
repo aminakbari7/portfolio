afa6c7f98e28eb785074f24f53b447ea0080c65f
aminakbari7 <aminakbari733@gmail.com>
2024-02-15 08:55:58 +0330

    Update createproject.blade.php

--- resources/views/admin/createproject.blade.php
+++ resources/views/admin/createproject.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
-  <head>
-
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>Admin Panel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+     <link href="../styles/style.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css')}}">
 
@@ -17,104 +18,50 @@
     <link rel="stylesheet" href="{{ asset('assets/css/owl-carousel.css')}}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css')}}">
+</head>
 
-    <title>my job</title>
-<!--
-
-
--->
-    <!-- Additional CSS Files -->
-  
-
-    </head>
-    
-    <body>
-    
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>  
+<body>
 @include('layouts.navigation')
-    
+@livewire('createp')
 
+  <script src="{{asset('assets/js/jquery-2.min.js')}}"></script>
 
-
-    <!-- ***** About Area Starts ***** -->
-    <section class="section" id="reservation" style="background-color: rgb(140, 173, 173);margin-top:-10px" dir="rtl" >
-       @livewire('createp')
-    </section>
-
-
-
-
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-xs-12">
-                    <div class="right-text-content">
-                            <ul class="social-icons">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                            </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    
-                </div>
-                <div class="col-lg-4 col-xs-12">
-                    <div class="left-text-content">
-                        <p>© Copyright Amin akbari
-                        
-                        <br>Design: amin akbari</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <script src="{{asset('assets/js/jquery-2.min.js')}}"></script>
-
-    <!-- Bootstrap -->
-    <script src="{{ asset('assets/js/popper.js')}}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
-    <!-- Plugins -->
-    <script src="{{ asset('assets/js/owl-carousel.js')}}"></script>
-    <script src="{{ asset('assets/js/accordions.js')}}"></script>
-    <script src="{{ asset('assets/js/datepicker.js')}}"></script>
-    <script src="{{ asset('assets/js/scrollreveal.min.js')}}"></script>
-    <script src="{{ asset('assets/js/waypoints.min.js')}}"></script>
-    <script src="{{ asset('assets/js/jquery.counterup.min.js')}}"></script>
-    <script src="{{ asset('assets/js/imgfix.min.js')}}"></script> 
-    <script src="{{ asset('assets/js/slick.js')}}"></script> 
-    <script src="{{ asset('assets/js/lightbox.js')}}"></script> 
-    <script src="{{ asset('assets/js/isotope.js')}}"></script> 
-    
-    <!-- Global Init -->
-    <script src="{{ asset('assets/js/custom.js')}}"></script>
-    <script>
-    
-        $(function() {
-            var selectedClass = "";
-            $("p").click(function(){
-            selectedClass = $(this).attr("data-rel");
-            $("#portfolio").fadeTo(50, 0.1);
-                $("#portfolio div").not("."+selectedClass).fadeOut();
-            setTimeout(function() {
-              $("."+selectedClass).fadeIn();
-              $("#portfolio").fadeTo(50, 1);
-            }, 500);
-                
-            });
-        });
-    
-    </script>
-
+  <!-- Bootstrap -->
+  <script src="{{ asset('assets/js/popper.js')}}"></script>
+  <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
+  <!-- Plugins -->
+  <script src="{{ asset('assets/js/owl-carousel.js')}}"></script>
+  <script src="{{ asset('assets/js/accordions.js')}}"></script>
+  <script src="{{ asset('assets/js/datepicker.js')}}"></script>
+  <script src="{{ asset('assets/js/scrollreveal.min.js')}}"></script>
+  <script src="{{ asset('assets/js/waypoints.min.js')}}"></script>
+  <script src="{{ asset('assets/js/jquery.counterup.min.js')}}"></script>
+  <script src="{{ asset('assets/js/imgfix.min.js')}}"></script> 
+  <script src="{{ asset('assets/js/slick.js')}}"></script> 
+  <script src="{{ asset('assets/js/lightbox.js')}}"></script> 
+  <script src="{{ asset('assets/js/isotope.js')}}"></script> 
   
-  </body>
+  <!-- Global Init -->
+  <script src="{{ asset('assets/js/custom.js')}}"></script>
+  <script>
+  
+      $(function() {
+          var selectedClass = "";
+          $("p").click(function(){
+          selectedClass = $(this).attr("data-rel");
+          $("#portfolio").fadeTo(50, 0.1);
+              $("#portfolio div").not("."+selectedClass).fadeOut();
+          setTimeout(function() {
+            $("."+selectedClass).fadeIn();
+            $("#portfolio").fadeTo(50, 1);
+          }, 500);
+              
+          });
+      });
+  
+  </script>
+<script type="text/javascript">
+
+</script>
+</body>
 </html>
