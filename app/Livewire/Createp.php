e7679366750760a24cf32fb9995bd6abb33080c5
aminakbari7 <aminakbari733@gmail.com>
2024-02-15 08:55:56 +0330

    Update Createp.php

--- app/Livewire/Createp.php
+++ app/Livewire/Createp.php
@@ -1,11 +1,48 @@
 <?php
 
 namespace App\Livewire;
-
+use Livewire\WithFileUploads;
+use App\Models\web;
+use App\Models\machinelearning;
 use Livewire\Component;
 
 class Createp extends Component
 {
+    use WithFileUploads;
+    public $image ;
+    public $title ;
+    public $link;
+    public $body ;
+    public $case;
+    public function save()
+    {
+
+        $namei = md5($this->image . microtime()).'.'.$this->image->extension();
+        $this->image->storeAs('images', $namei,'public');
+    if($this->case=='web-app')
+    {
+        web::Create([
+        'body'=>$this->body,
+       'image'=>$namei,
+        'title'=>$this->title,
+        'link'=>$this->link]);
+        Session()->flash('msg', 'اضافه شد!'); 
+        $this->render();
+    }
+
+     if($this->case=='machine-learning')
+     {
+         machinelearning::Create([
+            'body'=>$this->body,
+            'image'=>$namei,
+             'title'=>$this->title,
+             'link'=>$this->link]);
+        Session()->flash('msg', 'اضافه شد!'); 
+        $this->render();
+    }
+
+    }
+    
     public function render()
     {
         return view('livewire.createp');
