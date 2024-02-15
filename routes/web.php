d4daad8f3db79e7c1875c2309916d9b446ea5939
aminakbari7 <aminakbari733@gmail.com>
2024-02-15 09:51:04 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\isadmin;
 use App\Livewire\Createp;
+use App\Livewire\Showp;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,10 +25,16 @@ Route::get('/', function () {return view('welcome');})->name('welcome');
 Route::get('/admin/createproject', function () {
     return view('/admin/createproject');
 })->middleware(['auth', 'verified','isadmin'])->name('createproject');
+Route::get('/admin/showprojects', function () {
+    return view('/admin/showprojects');
+})->middleware(['auth', 'verified','isadmin'])->name('showprojects');
 
 
 
 Route::get('/createp',Createp::class)->middleware(['auth', 'verified','isadmin']);
+Route::get('/showp',Showp::class)->middleware(['auth', 'verified','isadmin']);
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
