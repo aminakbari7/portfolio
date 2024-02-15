93b331e8999afb68fe51b9a9c042819fba5bf688
aminakbari7 <aminakbari733@gmail.com>
2024-02-15 09:56:49 +0330

    Update Showp.php

--- app/Livewire/Showp.php
+++ app/Livewire/Showp.php
@@ -9,6 +9,16 @@ use App\Models\machinelearning;
 
 class Showp extends Component
 {
+    public $del;
+    public function deletep($id)
+    {
+        $this->del=$id;
+        machinelearning::destroy( $this->del);
+           
+        Session()->flash('msg', 'حذف شد!'); 
+        $ml=machinelearning::all();
+        return view('livewire.showp',['ml'=>$ml]);
+    }
     public function render()
     {
         
