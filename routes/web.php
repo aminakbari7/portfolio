43afa96ec066f47787be1997e566c6fe7bf88735
aminakbari7 <aminakbari733@gmail.com>
2024-02-16 09:20:39 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\isadmin;
 use App\Livewire\Adminproject;
+use App\Livewire\Adminusers;
 use App\Livewire\Createp;
 use App\Livewire\Showp;
 
@@ -23,18 +24,21 @@ Route::get('/', function () {return view('welcome');})->name('welcome');
 
 
 
-Route::get('/admin/createproject', function () {
-    return view('/admin/createproject');
+
+
+Route::get('/admin/createproject', function () {return view('/admin/createproject');
 })->middleware(['auth', 'verified','isadmin'])->name('createproject');
 Route::get('/admin/showprojects', function () {
     return view('/admin/showprojects');
 })->middleware(['auth', 'verified','isadmin'])->name('showprojects');
-
+Route::get('/admin/showusers', function () {
+    return view('/admin/showusers');
+})->middleware(['auth', 'verified','isadmin'])->name('showusers');
 
 
 Route::get('/createp',Createp::class)->middleware(['auth', 'verified','isadmin']);
 Route::get('/adminproject',Adminproject::class)->middleware(['auth', 'verified','isadmin']);
-
+Route::get('/adminusers',Adminusers::class)->middleware(['auth', 'verified','isadmin']);
 
 
 Route::get('/dashboard', function () {
