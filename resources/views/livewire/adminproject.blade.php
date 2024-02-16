484c11ea1fb2d035e2c3412cecb204c895198150
aminakbari7 <aminakbari733@gmail.com>
2024-02-16 09:24:51 +0330

    Update adminproject.blade.php

--- resources/views/livewire/adminproject.blade.php
+++ resources/views/livewire/adminproject.blade.php
@@ -12,7 +12,7 @@
                     <th scope="col">delete</th>
                     <th scope="col">update</th>
                      <th scope="col">
-                       <div class="navbar navbar-expand-lg navbar-light">
+                       <div class=" navbar-expand-lg navbar-light">
                          <div class="collapse navbar-collapse" id="navbarSupportedContent">
                            <input  wire:model="searchuser" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                            <button wire:click="searchu()"class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
